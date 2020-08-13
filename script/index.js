604f2e7662f0e6a89ed1c7e09ca5e0321d78ac6a
landepa <297956615@qq.com>
2020-08-13 08:50:37 +0800

    footer-ing

--- script/index.js
+++ script/index.js
@@ -1,5 +1,16 @@
-console.log( '执行index.js中的命令' );
+console.log('执行index.js中的命令');
 
-// setTimeout(()=>{
-//     alert('欢迎来到首页')
-// },2000)
+
+$(function () {
+    // do something
+    /**
+ * @description: 商城app下载 二维码 显示与隐藏
+ */
+    $('.app_tit').mouseenter(() => {
+        console.log(1);
+        $('.app_wrap').toggle()
+    })
+    $('.app_tit').mouseleave(() => {
+        $('.app_wrap').toggle()
+    })
+});
