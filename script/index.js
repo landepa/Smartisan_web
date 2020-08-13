3c5637efa7aad62acd8540b9e322a481ad6acc4d
landepa <297956615@qq.com>
2020-08-13 22:06:16 +0800

    下拉菜单 ing

--- script/index.js
+++ script/index.js
@@ -1,16 +1,80 @@
-console.log('执行index.js中的命令');
+// 首页js操作
+"use strict";
+$(function () {//ready
+    console.log('执行index.js中的命令');
 
+    var $app_tit = $('.app_tit');
+    var $app_wrap = $('.app_wrap');
 
-$(function () {
-    // do something
+    var $head_cart = $('.head_cart');
+    var $head_cart_empty = $('.head_cart_empty');
+
+    var $search_as = $('.title_search_wrap a');
+    var $search_ipt = $('.title_search_wrap input');
+
+    var $title_lis = $('.title_nav_list li');
+    var $tit_sec_container = $('.title_nav_sec_container');
+    var $title_wrap = $('.title_wrap');
+
+// header操作 
     /**
  * @description: 商城app下载 二维码 显示与隐藏
  */
-    $('.app_tit').mouseenter(() => {
-        console.log(1);
-        $('.app_wrap').toggle()
+    $app_tit.mouseenter(() => {
+        $app_wrap.toggle()
     })
-    $('.app_tit').mouseleave(() => {
-        $('.app_wrap').toggle()
+    $app_tit.mouseleave(() => {
+        $app_wrap.toggle()
     })
+
+    /**
+     * @description: 购物车的隐藏与显示
+     * @param {type} 
+     * @return {type} 
+     */
+    $head_cart.mouseenter(() => {
+        $head_cart_empty.toggle()
+    })
+    $head_cart.mouseleave(() => {
+        $head_cart_empty.toggle()
+    })
+
+// 导航栏操作
+
+/**
+ * @description: 搜索框聚焦时a标签的显示与隐藏
+ * @param {type} 
+ * @return {type} 
+ */
+$search_ipt.focus(()=>{
+    $search_as.toggle()
+})
+// TODO 搜索框
+$search_ipt.blur(()=>{
+    $search_as.toggle()
+})
+
+/**
+ * @description: 二级菜单的显示与隐藏
+ * @param {type} 
+ * @return {type} 
+ */
+$title_lis.mouseenter(()=>{
+    $tit_sec_container.show(1000);
+})
+$title_wrap.mouseleave(()=>{
+    $tit_sec_container.hide();
+})
+$tit_sec_container.mouseenter(()=>{
+    $tit_sec_container.show();
+})
+$tit_sec_container.mouseleave(()=>{
+    $tit_sec_container.hide();
+})
+
+
+
+
+
+
 });
