934a55d344565129a842832762e30a98326cf5dc
landepa <297956615@qq.com>
2020-08-14 21:57:39 +0800

    hot list ing

--- script/index.js
+++ script/index.js
@@ -1,76 +1,39 @@
 // 首页js操作
 "use strict";
 $(function () {//ready
+    $(".header_wrap").load("header.html");
+    $(".footer_wrap").load("footer.html");
+    $(".title_wrap").load("title_nav.html");
+
     console.log('执行index.js中的命令');
 
-    var $app_tit = $('.app_tit');
-    var $app_wrap = $('.app_wrap');
-
-    var $head_cart = $('.head_cart');
-    var $head_cart_empty = $('.head_cart_empty');
-
-    var $search_as = $('.title_search_wrap a');
-    var $search_ipt = $('.title_search_wrap input');
-
-    var $title_lis = $('.title_nav_list li');
-    var $tit_sec_container = $('.title_nav_sec_container');
-    var $title_wrap = $('.title_wrap');
-
-// header操作 
-    /**
- * @description: 商城app下载 二维码 显示与隐藏
- */
-    $app_tit.mouseenter(() => {
-        $app_wrap.toggle()
-    })
-    $app_tit.mouseleave(() => {
-        $app_wrap.toggle()
-    })
-
-    /**
-     * @description: 购物车的隐藏与显示
-     * @param {type} 
-     * @return {type} 
-     */
-    $head_cart.mouseenter(() => {
-        $head_cart_empty.toggle()
-    })
-    $head_cart.mouseleave(() => {
-        $head_cart_empty.toggle()
-    })
-
-// 导航栏操作
-
 /**
- * @description: 搜索框聚焦时a标签的显示与隐藏
+ * @description: 轮播图
  * @param {type} 
  * @return {type} 
- */
-$search_ipt.focus(()=>{
-    $search_as.toggle()
-})
-// TODO 搜索框
-$search_ipt.blur(()=>{
-    $search_as.toggle()
+ */    
+var mySwiper = new Swiper('.swiper-container', {
+    // direction: 'vertical', // 垂直切换选项
+    loop: true, // 循环模式选项
+    effect: 'fade',
+    // 如果需要分页器
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    autoplay: true, //等同于以下设置
+    /*autoplay: {
+        delay: 3000,
+        stopOnLastSlide: false,
+        disableOnInteraction: true,
+    },*/
+    // 分页器
+    pagination: {
+        el: '.swiper-pagination',
+        bulletActiveClass: 'my-bullet-active',
+    },
 })
 
-/**
- * @description: 二级菜单的显示与隐藏
- * @param {type} 
- * @return {type} 
- */
-$title_lis.mouseenter(()=>{
-    $tit_sec_container.show(1000);
-})
-$title_wrap.mouseleave(()=>{
-    $tit_sec_container.hide();
-})
-$tit_sec_container.mouseenter(()=>{
-    $tit_sec_container.show();
-})
-$tit_sec_container.mouseleave(()=>{
-    $tit_sec_container.hide();
-})
+
 
 
 
